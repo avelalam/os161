4fa97b236d36e8ef50c94668cb5686db57f1de18
Ubuntu <ubuntu@ip-172-31-6-242.us-west-2.compute.internal>
2017-03-02 02:55:25 +0000

    sys_getcwd

--- kern/include/file_syscall.h
+++ kern/include/file_syscall.h
@@ -4,3 +4,4 @@ int sys_open(char *filename,int flags);
 int sys_read(int fd, void* buf, int buflen);
 off_t sys_lseek(int fd,int low_32,int high_32,int whence);
 int sys_dup2(int oldfd,int newfd);
+int sys_getcwd(char *buf,size_t buflen);
--- kern/syscall/file_syscall.c
+++ kern/syscall/file_syscall.c
@@ -105,7 +105,7 @@ int sys_read(int fd, void* buf, int buflen){
 	uio_read.uio_space = proc_getas();
 	uio_read.uio_segflg = UIO_USERSPACE;
 	uio_read.uio_iovcnt = 1;
-	uio_read.uio_iov->iov_ubase = (userptr_t)buf;
+   	uio_read.uio_iov->iov_ubase = (userptr_t)buf;
 	uio_read.uio_iov->iov_len = len;
 	uio_read.uio_resid = len;
 	uio_read.uio_offset = curproc->file_table[fd]->offset;
@@ -170,7 +170,26 @@ int sys_dup2(int oldfd,int newfd){
 
 }
 
-
+int sys_getcwd(char *buf,size_t buflen){
+ 
+  
+      struct uio uio_getcwd;    
+      uio_getcwd.uio_iovcnt=1;
+      uio_getcwd.uio_offset=0;
+      uio_getcwd.uio_resid=buflen;
+      uio_getcwd.uio_rw=UIO_READ;
+      uio_getcwd.uio_space=proc_getas();
+      uio_getcwd.uio_iov->iov_kbase = (char *)buf;
+      uio_getcwd.uio_iov->iov_len = buflen;
+    int err=vfs_getcwd(&uio_getcwd);
+     if(err){
+           return err;
+     }
+   
+    return -(buflen-uio_getcwd.uio_resid);
+   
+ 
+}
 
 
 
